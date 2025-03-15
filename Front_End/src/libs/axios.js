0a4c852fb8b5589a9036bbeb22ce0169dbc1b0dd
ChenouWu <155577614+ChenouWu@users.noreply.github.com>
2025-03-15 16:33:34 -0300

    Update axios.js

--- Front_End/src/libs/axios.js
+++ Front_End/src/libs/axios.js
@@ -1,11 +1,50 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://secondhandmarket.onrender.com/api",
-    withCredentials: true,  
+    baseURL: "https://secondhandmarket.onrender.com/api/",
+    withCredentials: true,
     headers: {
         "Content-Type": "application/json",
     }
 });
+
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        
+        const token = document.cookie
+            .split('; ')
+            .find(row => row.startsWith('jwt='))
+            ?.split('=')[1];
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        if (config.url.startsWith('/')) {
+            config.url = config.url.substring(1);
+        }
+
+        console.log('Request Config:', {
+            url: config.baseURL + config.url,
+            headers: config.headers
+        });
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 404) {
+            console.error('API Endpoint not found:', error.config.url);
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance;
